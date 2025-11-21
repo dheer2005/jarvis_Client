8972af9a9543372d1f59725f39757e561d46b805
dhirendra <dhirendranyadav@gmail.com>
2025-11-21 17:33:50 +0530

    changesEnv

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 
 // src/environments/environment.ts (Development)
-export const environment = {
-  production: false,
-  serverUrl: 'https://localhost:7120'
-};
+// export const environment = {
+//   production: false,
+//   serverUrl: 'https://localhost:7120'
+// };
 
 // src/environments/environment.prod.ts (Production)
-// export const environment = {
-//   production: true,
-//   serverUrl: 'https://your-jarvis-server.azurewebsites.net' // Your deployed server URL
-// };
+export const environment = {
+  production: true,
+  serverUrl: 'https://jarvishserver001.bsite.net'
+};
